2db0d5971e5e46bcc42b0c6b7ad2c79c15858f99
Gurjant22 <gurjant@pariqsha.com>
2024-02-23 18:45:56 +0530

    deployed

--- app.py
+++ app.py
@@ -1,20 +1,27 @@
+import pandas as pd
+from werkzeug.utils import secure_filename
 from langchain.agents import Tool
 from langchain.chains import RetrievalQA
 from langchain.text_splitter import CharacterTextSplitter
-from langchain_community.document_loaders import PyPDFLoader
 from langchain_community.vectorstores import FAISS
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from pydantic import Field, BaseModel
 from langchain.agents import initialize_agent, AgentType
-from flask import Flask, request, jsonify
+from flask import Flask, render_template, request, jsonify
 from typing import Any
 import os
 from dotenv import load_dotenv
 from langchain.globals import set_debug
+from langchain_community.document_loaders import PyPDFLoader, CSVLoader
 from slugify import slugify
 set_debug(True)
 load_dotenv()
 app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 
 class DocumentInput(BaseModel):
@@ -23,35 +30,6 @@ class DocumentInput(BaseModel):
 
 llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo-0613",
                  openai_api_key=os.environ.get("OPENAI_API_KEY"))
-
-
-@app.route('/upload', methods=['POST'])
-def upload_files():
-    if 'file1' not in request.files or 'file2' not in request.files:
-        return jsonify({'error': 'Both file1 and file2 must be provided'})
-
-    file1 = request.files['file1']
-    file2 = request.files['file2']
-
-    os.remove("uploads/*")
-
-    if file1.filename == '' or file2.filename == '':
-        return jsonify({'error': 'Both file1 and file2 must be selected'})
-
-    filename1 = ("uploads/" + file1.filename)
-
-    filename2 = (
-        "uploads/" + file2.filename
-    )
-
-    file1.save(filename1)
-    file2.save(filename2)
-
-    return jsonify({'message': 'Files saved successfully'})
-
-
-tools = []
-path = "uploads/"
 
 
 def get_files_from_folder(folder_path):
@@ -63,37 +41,76 @@ def get_files_from_folder(folder_path):
     return files
 
 
-files = get_files_from_folder(path)
+#  store intialized agent in global variable
+agent = None
 
-print(files)
 
-for file in files:
-    loader = PyPDFLoader(file["path"])
-    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-    docs = loader.load_and_split(text_splitter=text_splitter)
-    embeddings = OpenAIEmbeddings()
-    retriever = FAISS.from_documents(docs, embeddings).as_retriever()
+@app.route('/upload', methods=['POST'])
+def upload_files():
+    print(request.files.lists, "list")
+    if 'files' not in request.files:
+        return jsonify({'error': 'No files were provided'})
 
-    # Wrap retrievers in a Tool
-    tools.append(
-        Tool(
-            args_schema=DocumentInput,
-            name=slugify(file["name"]),
-            description="Retrieve documents from uploaded PDF file",
-            func=RetrievalQA.from_chain_type(llm=llm, retriever=retriever),
+    uploaded_files = request.files.getlist('files')
+    print(uploaded_files)
+
+    if len(uploaded_files) < 2:
+        return jsonify({'error': 'At least two files must be selected'})
+
+    # delete all existing files in uploads folder
+    for file in os.listdir("uploads"):
+        os.remove(os.path.join("uploads", file))
+    for file in uploaded_files:
+        print(file.filename)
+        if file.filename == '':
+            return jsonify({'error': 'All files must have a filename'})
+
+        file.save(os.path.join("uploads", file.filename))
+
+    tools = []
+    path = "uploads"
+    files = get_files_from_folder(path)
+
+    for file in files:
+        if file["name"].endswith('.pdf'):
+            loader = PyPDFLoader(file["path"])
+        elif file["name"].endswith('.xls') or file["name"].endswith('.xlsx'):
+            loader = pd.read_excel(file["path"])
+        elif file["name"].endswith('.csv'):
+            loader = CSVLoader(file["path"])
+        else:
+            # Handle unsupported file types
+            continue
+
+        # Rest of the code remains the same
+        text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+        docs = loader.load_and_split(text_splitter=text_splitter)
+        embeddings = OpenAIEmbeddings()
+        retriever = FAISS.from_documents(docs, embeddings).as_retriever()
+
+        tools.append(
+            Tool(
+                args_schema=DocumentInput,
+                name=slugify(file["name"]),
+                description="Retrieve documents from uploaded file",
+                func=RetrievalQA.from_chain_type(llm=llm, retriever=retriever),
+            )
         )
+    global agent
+    agent = initialize_agent(
+        agent=AgentType.OPENAI_FUNCTIONS,
+        tools=tools,
+        llm=llm,
+        verbose=True,
     )
-agent = initialize_agent(
-    agent=AgentType.OPENAI_FUNCTIONS,
-    tools=tools,
-    llm=llm,
-    verbose=True,
-)
+
+    return jsonify({'message': 'Files saved successfully'})
 
 
 @app.route('/compare_documents', methods=['POST'])
 def compare_documents():
     data = request.get_json()
+    print(data, "data")
 
     if 'question' not in data:
         return jsonify({"error": "question field 'question' is required."}), 400
